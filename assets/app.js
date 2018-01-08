06f98bff1d216a1f3b509dfce4e7434a3d3e94d0
Magda Skowroń <mskowron0819@gmail.com>
2018-01-08 11:55:46 +0100

    slider fix

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,6 @@
 $( document ).ready(function() {
+	
+
 	var imgSrc = $('header');
 	var mq = window.matchMedia("(min-width: 576px)");
 	function widthChange(mq) {
@@ -28,28 +30,71 @@ $( document ).ready(function() {
 	var prev = $(".prev");
     var next = $(".next");
     var sliderImg = $(".slider li");
-
-    sliderImg.addClass('hidden-xs');
+    var slider = $(".slider ul");
+    
     sliderImg.find('h3').addClass('slider-underline');
     console.log($(".slider").find('.active').find('h3'));
-    
-
-    next.click(function() {
-    	
-    	if($(this).parent().find('li:last-child').hasClass('active')){
-    		$(this).parent().find('li:first-child').addClass('active');
-    	}
-    	var slideActive = $(this).parent().find('li.active');
-    	var h3 = slideActive.find('h3');
-    	var h3Next = slideActive.next().find('h3');
-    	
-    	slideActive.toggleClass('active');
-    	slideActive.next().addClass('active');
-    	h3.toggleClass('show');
-    	h3.next().toggleClass('show');
-    	h3Next.addClass('show');
-    	h3Next.next().addClass('show');
+    var liHeight = $(".slider li.active").height();
+	$(".slider li.show").css('height', liHeight);
+	$(".slider li").addClass('hidden');
+	
+	function sliderActive(mq){
+		if (mq.matches) {
+			$(".slider li").addClass('hidden');
+			$(".slider li").removeClass('show');
+			$(".slider li.active").next().toggleClass('show hidden');
+		    $(".slider li.active").next().next().toggleClass('show hidden');
+		    $(".slider li.active").prev().toggleClass('show hidden');
+		    $(".slider li.active").prev().prev().toggleClass('show hidden');
+		    $(".slider li.active").removeClass('hidden');	
+		}
+		var liHeight = $(".slider li.active").height();
+		$(".slider li.show").css('height', liHeight);
+	}
+	sliderActive(mq);
+	next.click(function() {
+    		var slideActive = $(this).parent().find('li.active');
+    		var firstSlide = $(this).parent().find('li:first-child');
+    		var firstSlideCopy = firstSlide.clone();
+    		var h3 = slideActive.find('h3');
+    		var h3Next = slideActive.next().find('h3');
+    		slideActive.removeClass('active');
+    		slideActive.next().addClass('active');
+    		h3.removeClass('show');
+    		h3.next().removeClass('show');
+	    	h3Next.addClass('show');
+	    	h3Next.next().addClass('show');
+	    	
+	    	firstSlide.remove();
+	    	slider.append(firstSlideCopy);
+	    	sliderActive(mq);
+	    	
+	    	
+    		
     });
+    // next.click(function() {
+    // 		var slideActive = $(this).parent().find('li.active');
+    // 		var h3 = slideActive.find('h3');
+    // 		var h3Next = slideActive.next().find('h3');
+    // 	if($(this).parent().find('li:last-child').hasClass('active')){
+    // 		slideActive.removeClass('active');
+    // 		$(this).parent().find('li:first-child').addClass('active');
+    // 		var newActive = $(this).parent().find('li.active');
+    // 		var newH3 = newActive.find('h3');
+    // 		h3.removeClass('show');
+    // 		h3.next().removeClass('show');
+    // 		newH3.addClass('show');
+    // 		newH3.next().addClass('show');
+    // 	}else{
+    // 		slideActive.removeClass('active');
+    // 		slideActive.next().addClass('active');
+    // 		h3.removeClass('show');
+    // 		h3.next().removeClass('show');
+	   //  	h3Next.addClass('show');
+	   //  	h3Next.next().addClass('show');
+    // 	}    	
+    // });
+
     prev.click(function () {
 		
     	if($(this).parent().find('li:first-child').hasClass('active')){
@@ -58,7 +103,7 @@ $( document ).ready(function() {
     	var slideActive = $(this).prev().find('li.active');
 		var h3 = slideActive.find('h3');
     	var h3Next = slideActive.prev().find('h3');
-    	
+
     	slideActive.toggleClass('active');
     	slideActive.prev().addClass('active');
     	h3.toggleClass('show');
